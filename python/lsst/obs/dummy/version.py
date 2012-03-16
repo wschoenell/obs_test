6d2c14e8be65a6dd50535aadfe30225205ad2840
Kian-Tat Lim <ktl@slac.stanford.edu>
2012-03-16 09:39:43 -0700

    Initial checkin of skeleton mapper package for use by tests.

---
+++ python/lsst/obs/dummy/version.py
@@ -0,0 +1,5 @@
+__version__ = '4.8.0.1-11-gde41ae3'
+__repo_version__ = '4.8.0.1-11-gde41ae3'
+__dependency_versions__ = {
+}
+__all__ = ('__version__', '__repo_version__', '__dependency_versions__')

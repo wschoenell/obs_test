0222e582b6fbf1c175c631a7495651037b266695
Kian-Tat Lim <ktl@slac.stanford.edu>
2012-03-17 00:37:38 -0700

    Complete mapper (except bypass functionality.  Add input data for raw.

--- python/lsst/obs/test/testMapper.py
+++ python/lsst/obs/test/testMapper.py
@@ -20,17 +20,137 @@
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 
+import os
 import re
 
 import lsst.daf.base as dafBase
 import lsst.daf.persistence as dafPersist
 
 class TestMapper(dafPersist.Mapper):
-    def __init__(self, **kwargs):
-        policyFile = pexPolicy.DefaultPolicyFile("obs_test", "TestMapper.paf", "policy")
-        policy = pexPolicy.Policy(policyFile)
+    def __init__(self, root=None, calibRoot=None, outputRoot=None, **kwargs):
+        super(TestMapper, self).__init__()
+        self.root = root
+        self.calibRoot = calibRoot
+        self.outputRoot = outputRoot
+        self.keyDict = dict(
+                skyTile=int,
+                visit=int,
+                raft=str,
+                sensor=str,
+                snap=int,
+                channel=str
+                )
+        self.levels = dict(
+                skyTile=['visit', 'raft', 'sensor', 'snap', 'channel'],
+                visit=['snap', 'raft', 'sensor', 'channel'],
+                raft=['snap', 'sensor', 'channel'],
+                sensor=['snap', 'channel'],
+                snap=['channel']
+                )
+        self.defaultLevel = 'sensor'
+        self.defaultSubLevels = dict(
+                skyTile='sensor',
+                visit='sensor',
+                raft='sensor',
+                sensor='channel'
+                )
+        self.dictList = []
+        for raft in ["0,1", "0,2", "0,3"]:
+            for sensor in ["0,1", "1,0", "1,1"]:
+                for snap in [0, 1]:
+                    for channel in ["0,0", "0,1", "1,0", "1,1"]:
+                        self.dictList.append(dict(
+                            skyTile=1,
+                            visit=85470982,
+                            raft=raft,
+                            sensor=sensor,
+                            snap=snap,
+                            channel=channel
+                            ))
 
-        super(DummyMapper, self).__init__(policy, policyFile.getRepositoryPath(), **kwargs)
+
+    def keys(self):
+        return self.keyDict.iterkeys()
+
+    def getKeys(self, datasetType, level):
+        keyDict = self.keyDict
+        if level is not None and level in self.levels:
+            keyDict = dict(keyDict)
+            for l in self.levels[level]:
+                if l in keyDict:
+                    del keyDict[l]
+        return keyDict
+
+    def getDefaultLevel(self):
+        return self.defaultLevel
+
+    def getDefaultSubLevel(self, level):
+        if level in self.defaultSubLevels:
+            return self.defaultSubLevels[level]
+        return None
 
     def validate(self, dataId):
+        for component in ("raft", "sensor", "channel"):
+            if component not in dataId:
+                continue
+            id = dataId[component]
+            if not isinstance(id, str):
+                raise RuntimeError, \
+                        "%s identifier should be type str, not %s: %s" % \
+                        (component.title(), type(id), repr(id))
+            if not re.search(r'^(\d),(\d)$', id):
+                raise RuntimeError, \
+                        "Invalid %s identifier: %s" % (component, repr(id))
+        for component in ("visit", "snap"):
+            if component not in dataId:
+                continue
+            id = dataId[component]
+            if not isinstance(id, int) and not isinstance(id, long):
+                raise RuntimeError, \
+                        "%s identifier should be int or long, not %s: %s" % \
+                        (component.title(), type(id), repr(id))
+            if component == "snap":
+                if id < 0 or id > 1:
+                    raise RuntimeError, \
+                            "Invalid snap identifier: %d" % (id,)
+
         return dataId
+
+    def map_raw(self, dataId):
+        loc = "raw-v%(visit)d-E%(snap)03d-r%(raft)s-s%(sensor)s-c%(channel)s.pickle" % dataId
+        loc = os.path.join(self.root, loc)
+        return dafPersist.ButlerLocation(None, "ImageU", "PickleStorage",
+                [loc], dataId)
+
+    def std_raw(self, object, dataId):
+        return str(object) + "/" + str(dataId['visit'])
+
+    def map_processCcd_config(self, dataId):
+        loc = "config-v%(visit)d-r%(raft)s-s%(sensor)s.py" % dataId
+        loc = os.path.join(self.root, loc)
+        return dafPersist.ButlerLocation(None, "Config", "ConfigStorage",
+                [loc], dataId)
+
+    def map_processCcd_metadata(self, dataId):
+        loc = "md-v%(visit)d-r%(raft)s-s%(sensor)s.boost" % dataId
+        loc = os.path.join(self.root, loc)
+        return dafPersist.ButlerLocation(
+                "lsst.daf.base.PropertySet", "PropertySet", "BoostStorage",
+                [loc], dataId)
+
+    def query_raw(self, level, format, dataId):
+        result = set()
+        for d in self.dictList:
+            where = True
+            for k in dataId.iterkeys():
+                if k not in d:
+                    raise RuntimeError("%s not in %s" % (k, repr(d)))
+                if d[k] != dataId[k]:
+                    where = False
+                    break
+            if where:
+                values = []
+                for k in format:
+                    values.append(d[k])
+                result.add(tuple(values))
+        return result
--- tests/map.py
+++ tests/map.py
@@ -45,11 +45,11 @@ class TestMapperTestCase(unittest.TestCase):
     """A test case for the test mapper."""
 
     def setUp(self):
-        self.input = os.path.join(os.environ['OBS_TEST'],
+        self.input = os.path.join(os.environ['OBS_TEST_DIR'],
                 "tests", "data", "input")
-        self.calib = os.path.join(os.environ['OBS_TEST'],
+        self.calib = os.path.join(os.environ['OBS_TEST_DIR'],
                 "tests", "data", "calib")
-        self.output = os.path.join(os.environ['OBS_TEST'],
+        self.output = os.path.join(os.environ['OBS_TEST_DIR'],
                 "tests", "data", "output")
         self.mapper = TestMapper(root=self.input,
                 calibRoot=self.calib,
@@ -58,49 +58,187 @@ class TestMapperTestCase(unittest.TestCase):
     def tearDown(self):
         del self.mapper
 
+
     def testMapRaw(self):
-        pass
+        dataId = dict(visit=85470982,
+                raft="0,3", sensor="0,1", channel="0,0", snap=1)
+        loc = self.mapper.map_raw(dataId)
+        self.assertEqual(loc.getPythonType(), None)
+        self.assertEqual(loc.getCppType(), "ImageU")
+        self.assertEqual(loc.getStorageName(), "PickleStorage")
+        self.assertEqual(loc.getLocations(), [os.path.join(self.input,
+            "raw-v85470982-E001-r0,3-s0,1-c0,0.pickle")])
+        for k, v in dataId.iteritems():
+            self.assertEqual(loc.getAdditionalData().get(k), v)
 
     def testQueryRawData(self):
-        pass
+        tuples = self.mapper.query_raw("raft", ["visit", "raft"], dict())
+        self.assertEqual(tuples, set([
+            (85470982, "0,1"), (85470982, "0,2"), (85470982, "0,3"),
+            ]))
+
+        tuples = self.mapper.query_raw("channel",
+                ["raft", "sensor", "channel"],
+                dict(visit=85470982, raft="0,1"))
+        self.assertEqual(tuples, set([
+            ("0,1", "0,1", "0,0"),
+            ("0,1", "0,1", "0,1"),
+            ("0,1", "0,1", "1,0"),
+            ("0,1", "0,1", "1,1"),
+            ("0,1", "1,0", "0,0"),
+            ("0,1", "1,0", "0,1"),
+            ("0,1", "1,0", "1,0"),
+            ("0,1", "1,0", "1,1"),
+            ("0,1", "1,1", "0,0"),
+            ("0,1", "1,1", "0,1"),
+            ("0,1", "1,1", "1,0"),
+            ("0,1", "1,1", "1,1"),
+            ]))
 
     def testStdRawData(self):
-        pass
+        dataId = dict(visit=85470982,
+                snap=1, raft="0,2", sensor="1,0", channel="0,1")
+        self.assertEqual(self.mapper.std_raw(210011, dataId),
+                '210011/85470982')
 
     def testMapConfigData(self):
-        pass
-
-    def testQueryConfigData(self):
-        pass
+        dataId = dict(visit=85470982, raft="0,2", sensor="1,1")
+        loc = self.mapper.map_processCcd_config(dataId)
+        self.assertEqual(loc.getPythonType(), None)
+        self.assertEqual(loc.getCppType(), "Config")
+        self.assertEqual(loc.getStorageName(), "ConfigStorage")
+        self.assertEqual(loc.getLocations(), [os.path.join(self.input,
+            "config-v85470982-r0,2-s1,1.py")])
+        for k, v in dataId.iteritems():
+            self.assertEqual(loc.getAdditionalData().get(k), v)
 
     def testMapMetadataData(self):
-        pass
-
-    def testQueryMetadataData(self):
-        pass
+        dataId = dict(visit=85470982, raft="0,1", sensor="1,0")
+        loc = self.mapper.map_processCcd_metadata(dataId)
+        self.assertEqual(loc.getPythonType(), "lsst.daf.base.PropertySet")
+        self.assertEqual(loc.getCppType(), "PropertySet")
+        self.assertEqual(loc.getStorageName(), "BoostStorage")
+        self.assertEqual(loc.getLocations(), [os.path.join(self.input,
+            "md-v85470982-r0,1-s1,0.boost")])
+        for k, v in dataId.iteritems():
+            self.assertEqual(loc.getAdditionalData().get(k), v)
 
     def testKeys(self):
-        self.assertEqual(self.mapper.keys(),
-                set(['visit', 'raft', 'sensor', 'snap', 'channel']))
+        self.assertEqual(set(self.mapper.keys()),
+                set(['skyTile', 'visit', 'raft', 'sensor', 'snap', 'channel']))
 
     def testGetDatasetTypes(self):
-        self.assertEqual(self.mapper.getDatasetTypes(),
+        self.assertEqual(set(self.mapper.getDatasetTypes()),
                 set(['raw', 'processCcd_config', 'processCcd_metadata']))
 
+    def testGetKeys(self):
+        self.assertEqual(set(self.mapper.getKeys("raw", "skyTile")),
+                set(["skyTile"]))
+        self.assertEqual(set(self.mapper.getKeys("raw", "visit")),
+                set(["skyTile", "visit"]))
+        self.assertEqual(set(self.mapper.getKeys("raw", "raft")),
+                set(["skyTile", "visit", "raft"]))
+        self.assertEqual(set(self.mapper.getKeys("raw", "sensor")),
+                set(["skyTile", "visit", "raft", "sensor"]))
+        self.assertEqual(set(self.mapper.getKeys("raw", "snap")),
+                set(["skyTile", "visit", "raft", "sensor", "snap"]))
+        self.assertEqual(set(self.mapper.getKeys("raw", "channel")),
+                set(["skyTile", "visit", "raft", "sensor", "snap", "channel"]))
+
+    def testGetDefaultLevel(self):
+        self.assertEqual(self.mapper.getDefaultLevel(), "sensor")
+
+    def testGetDefaultSubLevel(self):
+        self.assertEqual(self.mapper.getDefaultSubLevel("skyTile"), "sensor")
+        self.assertEqual(self.mapper.getDefaultSubLevel("visit"), "sensor")
+        self.assertEqual(self.mapper.getDefaultSubLevel("raft"), "sensor")
+        self.assertEqual(self.mapper.getDefaultSubLevel("snap"), None)
+        self.assertEqual(self.mapper.getDefaultSubLevel("sensor"), "channel")
+        self.assertEqual(self.mapper.getDefaultSubLevel("channel"), None)
+
     def testMap(self):
-        pass
+        dataId = dict(visit=85470982,
+                raft="0,2", sensor="0,1", channel="1,1", snap=0)
+        loc = self.mapper.map("raw", dataId)
+        self.assertEqual(loc.getPythonType(), None)
+        self.assertEqual(loc.getCppType(), "ImageU")
+        self.assertEqual(loc.getStorageName(), "PickleStorage")
+        self.assertEqual(loc.getLocations(), [os.path.join(self.input,
+            "raw-v85470982-E000-r0,2-s0,1-c1,1.pickle")])
+        for k, v in dataId.iteritems():
+            self.assertEqual(loc.getAdditionalData().get(k), v)
+
+        dataId = dict(visit=85470982, raft="0,3", sensor="1,1")
+        loc = self.mapper.map("processCcd_config", dataId)
+        self.assertEqual(loc.getPythonType(), None)
+        self.assertEqual(loc.getCppType(), "Config")
+        self.assertEqual(loc.getStorageName(), "ConfigStorage")
+        self.assertEqual(loc.getLocations(), [os.path.join(self.input,
+            "config-v85470982-r0,3-s1,1.py")])
+        for k, v in dataId.iteritems():
+            self.assertEqual(loc.getAdditionalData().get(k), v)
 
     def testQueryMetadata(self):
-        pass
+        tuples = self.mapper.queryMetadata("raw", "sensor",
+                ["skyTile", "visit", "raft", "sensor"],
+                dict(raft="0,3", sensor="0,1", visit=85470982))
+        self.assertEqual(tuples, set([(1, 85470982, "0,3", "0,1")]))
+
+        tuples = self.mapper.queryMetadata("raw", "sensor",
+                ["skyTile", "visit", "raft", "sensor"], dict(visit=85470982))
+        self.assertEqual(tuples, set([
+            (1, 85470982, "0,1", "0,1"),
+            (1, 85470982, "0,1", "1,0"),
+            (1, 85470982, "0,1", "1,1"),
+            (1, 85470982, "0,2", "0,1"),
+            (1, 85470982, "0,2", "1,0"),
+            (1, 85470982, "0,2", "1,1"),
+            (1, 85470982, "0,3", "0,1"),
+            (1, 85470982, "0,3", "1,0"),
+            (1, 85470982, "0,3", "1,1")]))
 
     def testCanStandardize(self):
-        pass
+        self.assertEqual(self.mapper.canStandardize("raw"), True)
+        self.assertEqual(self.mapper.canStandardize("processCcd_config"),
+                False)
+        self.assertEqual(self.mapper.canStandardize("processCcd_metadata"),
+                False)
 
     def testStandardize(self):
-        pass
+        dataId = dict(visit=12345)
+        self.assertEqual(self.mapper.standardize("raw", 9876, dataId),
+                '9876/12345')
 
     def testValidate(self):
-        pass
+        for dataId in [
+                dict(visit=85470982,
+                    raft="0,3", sensor="0,1", channel="0,0", snap=1),
+                dict(visit=85470982, raft="0,2", sensor="1,1"),
+                dict(visit=85470982, raft="0,1", sensor="1,0"),
+                dict(visit=85470982,
+                    raft="0,2", sensor="0,1", channel="1,1", snap=0),
+                dict(visit=85470982, raft="0,3", sensor="1,1"),]:
+            self.assertEqual(self.mapper.validate(dataId), dataId)
+        for dataId in [
+                dict(visit=85470982,
+                    raft="0.3", sensor="0,1", channel="0,0", snap=1),
+                dict(visit=85470982, raft="0,2", sensor="11"),
+                dict(visit=85470982, raft="x,1", sensor="1,0"),
+                dict(visit=85470982,
+                    raft="0,2", sensor="0,1", channel="1,1", snap="0"),
+                dict(visit="85470982", raft="0,3", sensor="1,1"),
+                dict(visit=85470982,
+                    raft=03, sensor="0,1", channel="0,0", snap=1),
+                ]:
+            try:
+                self.mapper.validate(dataId)
+                self.assert_(False,
+                        msg="%s failed to raise exception" % str(dataId))
+            except AssertionError:
+                raise
+            except Exception, e:
+                self.assertIsInstance(e, RuntimeError)
+
 
 def suite():
     utilsTests.init()

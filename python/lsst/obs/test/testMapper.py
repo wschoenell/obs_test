d8c103854f9c8a4bf2ce0e236508386ae356ed0c
Russell Owen <rowen@uw.edu>
2015-09-03 14:33:45 -0700

    Updated to use CameraMapper._makeCamera

--- python/lsst/obs/test/testMapper.py
+++ python/lsst/obs/test/testMapper.py
@@ -57,8 +57,6 @@ class TestMapper(CameraMapper):
         afwImageUtils.defineFilter('z', 866.85)
         afwImageUtils.defineFilter('y', 971.68, alias=['y4']) # official y filter
 
-        self.camera = TestCamera()
-
     def _extractDetectorName(self, dataId):
         return "0"
 
@@ -94,3 +92,9 @@ class TestMapper(CameraMapper):
     def _setCcdExposureId(self, propertyList, dataId):
         propertyList.set("Computed_ccdExposureId", self._computeCcdExposureId(dataId))
         return propertyList
+
+    def _makeCamera(self, policy, repositoryDir):
+        """Make a camera (instance of lsst.afw.cameraGeom.Camera) describing the camera geometry
+        """
+        return TestCamera()
+

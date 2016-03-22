5cfa1fd50f448bec504dc772c6e906da099ae6bd
Nate Pease <npease@slac.stanford.edu>
2016-03-22 14:42:22 -0700

    adds needed arguments for the butler cfg parser

--- python/lsst/obs/test/testMapper.py
+++ python/lsst/obs/test/testMapper.py
@@ -1,7 +1,7 @@
-# 
+#
 # LSST Data Management System
 # Copyright 2008, 2009, 2010 LSST Corporation.
-# 
+#
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
 #
@@ -9,14 +9,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the LSST License Statement and 
-# the GNU General Public License along with this program.  If not, 
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 
@@ -33,7 +33,7 @@ __all__ = ["TestMapper"]
 class TestMapper(CameraMapper):
     packageName = 'obs_test'
 
-    def __init__(self, inputPolicy=None, **kwargs):
+    def __init__(self, root, inputPolicy=None, **kwargs):
         policyFile = pexPolicy.DefaultPolicyFile(self.packageName, "testMapper.paf", "policy")
         policy = pexPolicy.Policy(policyFile)
 
@@ -45,7 +45,7 @@ class TestMapper(CameraMapper):
                 else:
                     kwargs[kw] = inputPolicy.get(kw)
 
-        CameraMapper.__init__(self, policy, policyFile.getRepositoryPath(), **kwargs)
+        CameraMapper.__init__(self, policy, policyFile.getRepositoryPath(), root=root, **kwargs)
         self.filterIdMap = {
                 'u': 0, 'g': 1, 'r': 2, 'i': 3, 'z': 4, 'y': 5, 'i2': 5}
 

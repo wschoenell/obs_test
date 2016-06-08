69d9b07f7f876e12cdc1ecd360f3672e3b567ea7
Russell Owen <rowen@uw.edu>
2016-06-08 15:42:34 -0700

    Update linearity info in amp info catalogs
    There is no linearity correction for obs_test, but the amp info
    catalogs said otherwise. This should have been fixed in DM-5462.

--- python/lsst/obs/test/testCamera.py
+++ python/lsst/obs/test/testCamera.py
@@ -23,6 +23,7 @@ import numpy
 import lsst.afw.cameraGeom as cameraGeom
 import lsst.afw.geom as afwGeom
 from lsst.afw.table import AmpInfoCatalog, AmpInfoTable, LL
+from lsst.afw.cameraGeom import NullLinearityType
 from lsst.afw.cameraGeom.cameraFactory import makeDetector
 
 class TestCamera(cameraGeom.Camera):
@@ -119,10 +120,9 @@ class TestCamera(cameraGeom.Camera):
         xRawExtent = xDataExtent + xBiasExtent
         yRawExtent = yDataExtent
         readNoise = 3.975 # amplifier read noise, in e-
-        linearityType = "PROPORTIONAL"
-        linearityThreshold = 0
-        linearityMax = 65535
-        linearityCoeffs = [linearityThreshold, linearityMax]
+        saturationLevel = 65535
+        linearityType = NullLinearityType
+        linearityCoeffs = [0, 0, 0, 0]
 
         schema = AmpInfoTable.makeMinimalSchema()
 
@@ -175,7 +175,7 @@ class TestCamera(cameraGeom.Camera):
                 record.setReadoutCorner(readCorner)
                 record.setGain(gain)
                 record.setReadNoise(readNoise)
-                record.setSaturation(linearityMax)
+                record.setSaturation(saturationLevel)
                 record.setSuspectLevel(float("nan"))
                 record.setLinearityCoeffs([float(val) for val in linearityCoeffs])
                 record.setLinearityType(linearityType)

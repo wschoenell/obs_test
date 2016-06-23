bbbebf3b8ce672ce1444da2cde1871b3a5f37c9c
Jim Bosch <jbosch@astro.princeton.edu>
2016-06-23 16:50:56 +0200

    Split CalibrateTask configuration into a separate file.

--- config/processCcd.py
+++ config/processCcd.py
@@ -5,6 +5,6 @@ from lsst.utils import getPackageDir
 
 configDir = os.path.join(getPackageDir("obs_test"), "config")
 config.isr.load(os.path.join(configDir, 'isr.py'))
-# we don't have astrometry_net data (yet) so astrom and photo cal are impossible
-config.calibrate.doAstrometry=False
-config.calibrate.doPhotoCal=False
+
+configDir = os.path.join(getPackageDir("obs_test"), "config")
+config.calibrate.load(os.path.join(configDir, 'calibrate.py'))
